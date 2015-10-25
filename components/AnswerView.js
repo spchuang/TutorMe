c83bee76b7b796e4824eb04ae7f758a796df5207
Sam Chuang <spchuang@spchuang-mbp.dhcp.thefacebook.com>
2015-10-24 17:38:21 -0700

    add button to answer view

--- components/AnswerView.js
+++ components/AnswerView.js
@@ -16,6 +16,9 @@ var {
   TextInput,
 } = React;
 
+var Button = require('react-native-button');
+var { Icon, } = require('react-native-icons');
+
 var AnswerView = React.createClass({
   
   getInitialState(): Object {
@@ -27,14 +30,32 @@ var AnswerView = React.createClass({
   render(): $jsx {
     return (
       <View style={styles.container}>
-         <TextInput
+        <Text>Comments:</Text>
+        <TextInput
           style={styles.input}
           onChangeText={(text) => this.setState({text})}
           multiline={true}
           value={this.state.text}
         />
+        <View style={styles.row}>
+          <View style={styles.buttonWrap}>
+            <Button onPress={this.submitQuestion}>
+              <Icon
+                name='ion|android-send'
+                size={30}
+                color='white'
+                style={styles.buttonIcon}
+              />
+              <Text style={styles.buttonText}>Answer</Text>
+            </Button>
+          </View>
+        </View>
       </View>
     );
+  },
+
+  submitQuestion(): void {
+
   },
 });
 
@@ -46,10 +67,32 @@ var styles = StyleSheet.create({
   input: {
     fontSize: 20,
     padding: 10,
-    height: 200,
+    height: 250,
     borderColor: 'gray', 
     borderWidth: 1,
   },
+
+  row: {
+    alignItems: 'center',
+  },
+  buttonWrap: {
+    marginTop: 5,
+    borderRadius: 8,
+    padding: 10,
+    width: 200,
+    backgroundColor: 'DB3340',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    fontFamily: 'HelveticaNeue-Medium',
+    marginRight: 50,
+  },
+  buttonIcon: {
+    width: 40,
+    height: 40,
+    marginLeft: 10,
+  }
 });
 
 module.exports = AnswerView;
